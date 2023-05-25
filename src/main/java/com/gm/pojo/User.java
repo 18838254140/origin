e086be82ab670a86061749b71088c8b61714a26b
zuiook <981933880@qq.com>
2023-05-25 11:48:44 +0800

    修改配置文件为yml，添加实体类user

---
+++ src/main/java/com/gm/pojo/User.java
@@ -0,0 +1,16 @@
+package com.gm.pojo;
+
+import lombok.Data;
+
+/**
+ * @author: gm
+ * @createTime 2023/5/25 11:47
+ * @description:
+ */
+
+@Data
+public class User {
+
+ private Integer id;
+ private String username;
+}
