60166c6d00714c7936360b256251d7f7a4a9a752
zuiook <981933880@qq.com>
2023-05-25 13:27:33 +0800

    添加controller

---
+++ src/main/java/com/gm/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.gm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author: gm
+ * @createTime 2023/5/25 13:26
+ * @description:
+ */
+
+@Controller
+public class UserController {
+
+ public void test(){
+
+ }
+}
