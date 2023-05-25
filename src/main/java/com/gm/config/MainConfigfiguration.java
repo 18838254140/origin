13c005fb80d35f67c4c3ea662e115e6d395fa6bf
zuiook <981933880@qq.com>
2023-05-25 11:46:40 +0800

    add config

---
+++ src/main/java/com/gm/config/MainConfigfiguration.java
@@ -0,0 +1,13 @@
+package com.gm.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: gm
+ * @createTime 2023/5/25 11:45
+ * @description:
+ */
+
+@Configuration
+public class MainConfigfiguration {
+}
